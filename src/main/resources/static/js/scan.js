a40c45d93b1f2fe498d51255c0fcb7d1b105ae04
Annie <atc16@sfu.ca>
2024-06-30 20:15:08 -0700

    added scanLink feature

--- src/main/resources/static/js/scan.js
+++ src/main/resources/static/js/scan.js
@@ -1,8 +1,15 @@
 const apiKey = '5796597306b3edb03cf9aa494b58a755431d95042f093fc3cc46375e52be9687';
-const scannedUrl = 'https://www.cercacor.com/';
+
+document.getElementById('formId').addEventListener('submit', scanLink);
+
+function scanLink() {
+    console.log("scanlink() is called");
+    scannedUrl = document.getElementById("scannedUrl");
+    sendUrl(scannedUrl).then(analysisId => getAnalysis(analysisId));
+} 
 
 // POST request to send scanned URL and get Analysis ID
-async function sendUrl(callback) {
+async function sendUrl(scannedUrl) {
     const request = new Request('https://www.virustotal.com/api/v3/urls', {
         method: 'POST',
         headers: {
@@ -34,12 +41,12 @@ async function getAnalysis(analysisId) {
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
+            console.log(data.data.attributes.stats);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
 }
 
-sendUrl().then(analysisId => getAnalysis(analysisId));
 
  
