6d85ea283a7ce30a2d80a5e95a5b17b564164d79
ichirojwang <ichiwang27@gmail.com>
2024-07-01 17:42:59 -0700

    style: editted some ui components

--- src/main/java/cmpt276/project/threatalert/controllers/WebsiteController.java
+++ src/main/java/cmpt276/project/threatalert/controllers/WebsiteController.java
@@ -1,8 +1,13 @@
 package cmpt276.project.threatalert.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import cmpt276.project.threatalert.models.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class WebsiteController {
