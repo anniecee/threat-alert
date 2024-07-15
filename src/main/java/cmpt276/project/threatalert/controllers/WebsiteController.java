ea5f98bc5211291fab4e701ec6d31f2fea6c2013
ehz2 <eltonzhen04@gmail.com>
2024-07-15 01:51:45 -0700

    big fixes: password restrictions, session management and feature: alerts for success & failure

--- src/main/java/cmpt276/project/threatalert/controllers/WebsiteController.java
+++ src/main/java/cmpt276/project/threatalert/controllers/WebsiteController.java
@@ -1,13 +1,8 @@
 package cmpt276.project.threatalert.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
-
-import cmpt276.project.threatalert.models.User;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class WebsiteController {
